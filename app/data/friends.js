9a1ea312acd8e042866691aae54bdd68a9c2bab7
Shelby England <Shelby.England@knights.ucf.edu>
2019-12-15 22:51:09 -0500

    pushing for help

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,34 +2,15 @@
 
 var friendsArray = [
     {
-      name:"Colton",
-      number: "number",
-      email: "email",
-      username: "username"
-  
-      // "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      // "scores":[]
+      name: "colton",
+      photo: "photo",
+      scores: [
+          3,
+          2,
+          2
+      ],
     }
   ]
 
-
-
-  $("#add-btn").on("click", function(event) {
-    event.preventDefault();
-    var newTables = {
-      name: $("#name").val().trim(),
-      number: $("#number").val().trim(),
-      email: $("#email").val().trim(),
-      unique: $("#username").val().trim()
-    };
-
-    console.log(newTables)
-  
-    $.post("/api/friebds", newTables)
-      .then(function(data) {
-        console.log("add.html", data);
-        alert("Adding Table...");
-      });
-  });
 
   module.exports = friendsArray;
