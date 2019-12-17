72f9b537fa3a281f2bada22faf63b2a92bb97cd1
Shelby England <Shelby.England@knights.ucf.edu>
2019-12-16 19:35:29 -0500

    added functionality for finding new friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,13 +4,48 @@ var friendsArray = [
     {
       name: "colton",
       photo: "photo",
-      scores: [
-          3,
-          2,
-          2
-      ],
+      scores: [3, 2, 2, 2, 1, 3, 3, 2, 2, 1]
+    },
+    {
+      name: "zack",
+      photo: "photo",
+      scores: [3, 3, 3, 2, 3, 3, 1, 2, 2, 1]
+    },
+    {
+      name: "cole",
+      photo: "photo",
+      scores: [2, 1, 1, 1, 1, 3, 1, 2, 2, 1]
+    },
+    {
+      name: "alex",
+      photo: "photo",
+      scores: [2, 2, 1, 2, 2, 1, 3, 2, 1, 1]
+    },
+    {
+      name: "dean",
+      photo: "photo",
+      scores: [3, 2, 1, 2, 1, 2, 3, 2, 2, 1]
+    },
+    {
+      name: "nick",
+      photo: "photo",
+      scores: [1, 2, 2, 1, 2, 3, 1, 2, 1, 1]
+    },
+    {
+      name: "ryan",
+      photo: "photo",
+      scores: [1, 1, 2, 3, 2, 3, 3, 1, 2, 1]
+    },
+    {
+      name: "matt",
+      photo: "photo",
+      scores: [3, 1, 1, 3, 1, 2, 3, 1, 2, 2]
+    },
+    {
+      name: "justin",
+      photo: "photo",
+      scores: [2, 1, 2, 3, 1, 3, 1, 3, 2, 1]
     }
   ]
-
 
   module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,10 +8,61 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
 
-        var newRes = req.body
+        let totalDifference = 0;
+        let bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        }
 
-        newRes.routeName = newRes.name.replace(/\s+/g, "").toLowerCase();    
-        friendsData.push(newRes);
+        let userData = req.body;
+        let userName = userData.name;
+        let userScores = userData.scores
+
+        let b = userScores.map(function(item) {
+            return parseInt(item, 10);
+        });
+
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: b
+        };
+
+        console.log(`Name: ${userName}`)
+        console.log(`Score: ${userScores}`)   
+
+        let sum = b.reduce((a, b) => a + b, 0);
+
+        console.log(`Sum of User Score is ${sum}`)
+        console.log(`Best Match Friend Difference ${bestMatch.friendDifference}`)
+        console.log("*-*-*-*-*-*-*-")
+
+        for (let i = 0 ; i < friendsData.length ; i++) {
+            console.log("for loop works")
+            console.log(friendsData[i].name)
+            totalDifference = 0;
+            console.log(`Total Difference ${totalDifference}`)
+            console.log(`Best Match friend difference ${bestMatch.friendDifference}`)
+
+            let bfriendScore = friendsData[i].scores.reduce((a, b) => a + b, 0);
+            console.log(`Total friend score ${bfriendScore}`)
+            totalDifference += Math.abs(sum - bfriendScore)
+            console.log(`Here is the total difference: ${totalDifference}`)
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                console.log("if statement works")
+                bestMatch.name = friendsData[i].name;
+                bestMatch.photo = friendsData[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            } else {
+                console.log("There is no one for you!")
+            }
+            console.log(`Here is the total difference: ${totalDifference}`)
+        }   
+            console.log(`Here is the best match: ${bestMatch.name}`)
+            friendsData.push(userData);
+            res.json(bestMatch)
       
       });
 
