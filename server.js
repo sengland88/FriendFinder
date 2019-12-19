1c39e28a5772ee7f0b3db3dab03e5d8874cddc3b
Shelby England <Shelby.England@knights.ucf.edu>
2019-12-19 00:08:44 -0500

    updating files

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
